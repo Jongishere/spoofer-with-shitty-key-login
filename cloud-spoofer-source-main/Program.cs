ee25dd95f71d0fed3f707ad52477671b4563f490
Jongers <61958546+Jongishere@users.noreply.github.com>
2022-06-12 17:20:06 -0400

    hi
    releasing because i have dropped this project a while ago

---
+++ cloud-spoofer-source-main/Program.cs
@@ -0,0 +1,125 @@
+﻿/// cloudy
+
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows;
+using JONGERS;
+
+namespace JONGERS
+{
+    /// there is the password u wanted bozo idc enough to make it a sql key database now try to crack my cheat :pepejam:
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username, password;
+            int ctr = 0;
+            Console.Write("\n\nLogin :\n");
+            Console.Write("------------------------------------------------------\n");
+
+            do
+            {
+                Console.Write("Input your key: ");
+                username = Console.ReadLine();
+
+
+                if (username != "2##azdacscasdawgsdfsdf3124@6869")
+                    ctr++;
+                else
+                    ctr = 1;
+
+            }
+            while ((username != "2##azdacscasdawgsdfsdf3124@6869") && (ctr != 9999999999999));
+
+            Console.WriteLine("Successfully Logged In");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("#####   ##      #####  ##   ## ######");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("##   ## ##     ##   ## ##   ## ##   ##");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("##      ##     ##   ## ##   ## ##   ##");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("##      ##     ##   ## ##   ## ##   ##");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("##      ##     ##   ## ##   ## ##   ##");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("##   ## ##     ##   ## ##   ## ##   ##");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(" #####  ####### #####   #####  ###### ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Press [Enter] to start");
+            Console.WriteLine();
+            Console.ReadKey();
+
+            ShowSimplePercentage();
+
+            Console.ReadKey();
+        }
+
+        static void ShowSimplePercentage()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write($" \rProgress: {i}%   ");
+                Thread.Sleep(25);
+            }
+
+            Console.Write("\r Done!          ");
+            Console.Clear();
+        Menu:
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine();
+            Console.Title = "[Cloud Spoofer] https://discord.gg/w4yKuCk7hT "; /// 
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("                                          ========================");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("                                                Cloud Spoofer");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("                                          ========================");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("                                           [!hwid] Spoof HWID");  /// start spoof
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(" [+] Select your option :  ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            string option = Console.ReadLine();
+
+            if (option == "!hwid")
+            {
+                MessageBox.Show(" Spoofing");
+                Spoofer.Spoof();
+                MessageBox.Show(" Done! Restart your pc bozo");
+                Environment.Exit(0);
+                goto Menu;
+            }
+            else if (option == "902")
+            {
+                MessageBox.Show(" Cleaning"); 
+                Spoofer.Clean();
+                MessageBox.Show(" Done!");
+                goto Menu;
+
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine(" Wrong button try again!");
+                Thread.Sleep(2000);
+                Console.Clear();
+                goto Menu;
+            }
+        }
+    }
+}
---
+++ cloud-spoofer-source-main/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("Cloud Spoofer")]
+[assembly: AssemblyDescription("Cloud Spoofer")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Spoofer")]
+[assembly: AssemblyCopyright("Copyright © Cloud 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("60bec678-a721-482d-a465-b8bca026e9d8")]
+[assembly: AssemblyVersion("6.0")]
+[assembly: AssemblyFileVersion("6.0")]
+
+
+/// ONE QUESTION? HOW MAD??///
